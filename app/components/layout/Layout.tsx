6a0de30b68ee6a6f16a7bb47cfe7bde6af89d0d8
Azmul Hosain <azmulg@gmail.com>
2022-01-11 22:58:59 +0600

    title changed

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -64,7 +64,7 @@ export default function LayoutComponent({ children }: Props) {
             collapsedWidth="0"
             className={styles.sidebar}
           >
-            <div className={styles.logo}>Ecommerce[Admin]</div>
+            <div className={styles.logo}>KUREGHOR[ADMIN]</div>
             <Navbar />
           </Sider>
          <Layout>
