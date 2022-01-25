f5dfcca6add9ae02e89c072c797d764af7e33997
Azmul Hosain <azmulg@gmail.com>
2022-01-25 23:14:17 +0600

    campaign and flash sell done

--- app/components/setting/Setting.tsx
+++ app/components/setting/Setting.tsx
@@ -9,11 +9,12 @@ import {
   Input,
   Button,
   Space,
-  Divider
+  Divider,
+  Checkbox,
 } from "antd";
 import * as SettingApi from "./SettingApi";
 import moment from "moment";
-import {capitalize} from "lodash";
+import { capitalize } from "lodash";
 
 export default function SettingPage() {
   const [loading, setloading] = useState(false);
@@ -22,48 +23,47 @@ export default function SettingPage() {
 
   const onFinish = async (values: any) => {
     setloading(true);
-    try{
+    try {
       await SettingApi.updateItem(values);
       message.success("Setting Updated successfully");
-    } finally{
+    } finally {
       setloading(false);
     }
   };
 
-  const getItems = useCallback(
-    async () => {
-      try {
-        setloading(true);
-        const item: any = await SettingApi.getItem();
-        setItem(item);
-        form.setFieldsValue({
-          call_us_number: item?.call_us_number,
-          free_delivery_money: item?.free_delivery_money,
-          contact_number1: item?.contact_number1,
-          contact_number2: item?.contact_number2,
-          business_email: item?.business_email,
-          business_site: item?.business_site,
-          business_main_address: item?.business_main_address,
-          facebook_link: item?.facebook_link,
-          youtube_link: item?.youtube_link,
-          twitter_link: item?.twitter_link,
-          instagram_link: item?.instagram_link,
-          pinterest_link: item?.pinterest_link,
-          bkash_number: item?.bkash_number,
-          nagad_number: item?.nagad_number,
-          rocket_number: item?.rocket_number,
-          bkash_discount: item?.bkash_discount,
-          nagad_discount: item?.nagad_discount,
-          rocket_discount: item?.rocket_discount
-        });
-      } catch (error: any) {
-        message.error(error?.message);
-      } finally {
-        setloading(false);
-      }
-    },
-    [form]
-  );
+  const getItems = useCallback(async () => {
+    try {
+      setloading(true);
+      const item: any = await SettingApi.getItem();
+      setItem(item);
+      form.setFieldsValue({
+        call_us_number: item?.call_us_number,
+        free_delivery_money: item?.free_delivery_money,
+        contact_number1: item?.contact_number1,
+        contact_number2: item?.contact_number2,
+        business_email: item?.business_email,
+        business_site: item?.business_site,
+        business_main_address: item?.business_main_address,
+        facebook_link: item?.facebook_link,
+        youtube_link: item?.youtube_link,
+        twitter_link: item?.twitter_link,
+        instagram_link: item?.instagram_link,
+        pinterest_link: item?.pinterest_link,
+        bkash_number: item?.bkash_number,
+        nagad_number: item?.nagad_number,
+        rocket_number: item?.rocket_number,
+        bkash_discount: item?.bkash_discount,
+        nagad_discount: item?.nagad_discount,
+        rocket_discount: item?.rocket_discount,
+        is_campaign_sell: item?.is_campaign_sell,
+        is_flash_sell: item?.is_flash_sell,
+      });
+    } catch (error: any) {
+      message.error(error?.message);
+    } finally {
+      setloading(false);
+    }
+  }, [form]);
 
   useEffect(() => {
     getItems();
@@ -74,7 +74,7 @@ export default function SettingPage() {
       <Spin spinning={loading}>
         <h3>Last updatedAt: {capitalize(moment(item?.updatedAt).fromNow())}</h3>
         <Form layout="vertical" form={form} onFinish={onFinish}>
-        <Divider orientation="left">Call Center Information</Divider>
+          <Divider orientation="left">Call Center Information</Divider>
           <Row justify="space-between">
             <Col xs={24} sm={11}>
               <Form.Item label="Call Us Number" name="call_us_number">
@@ -115,7 +115,7 @@ export default function SettingPage() {
           <Row justify="space-between">
             <Col xs={24} sm={11}>
               <Form.Item label="Nagad Number" name="nagad_number">
-                <Input type="number"  />
+                <Input type="number" />
               </Form.Item>
             </Col>
             <Col xs={24} sm={11}>
@@ -191,7 +191,24 @@ export default function SettingPage() {
               </Form.Item>
             </Col>
           </Row>
-
+          <Divider orientation="left">Sell Options</Divider>
+          <Row>
+            <Col xs={24} sm={8}>
+              <Form.Item
+                name="is_campaign_sell"
+                valuePropName="checked"
+                noStyle
+              >
+                <Checkbox>Campaign Sell</Checkbox>
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={8}>
+              <Form.Item name="is_flash_sell" valuePropName="checked" noStyle>
+                <Checkbox>Flash Sell</Checkbox>
+              </Form.Item>
+            </Col>
+          </Row>
+          <Divider />
           <Form.Item>
             <Space size="middle">
               <Button type="primary" htmlType="submit">
